0ac0a350d6b2f7c4b59a3608d17220e8c41025bd
Lukas Dutrieux <117676779+DutrieuxLU@users.noreply.github.com>
2026-05-23 15:26:15 -0500

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        max_k = max(piles)
+        min_k = 1
+        curr_solution = max_k
+        while max_k >= min_k:
+            curr = (max_k + min_k) // 2
+            test = 0
+            for pile in piles:
+                test += math.ceil(pile / curr)
+            if test <= h:
+                curr_solution = curr
+                max_k = curr - 1
+            else:
+                min_k = curr + 1
+        return curr_solution
